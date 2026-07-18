bfa9244d55db06f349f86ab2b1297fef3d772942
Ruben Rodrigues <rubenzidane08@gmail.com>
2026-07-18 10:25:07 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,11 @@ import NetworkStatus from '@/components/NetworkStatus';
 import IdleWarningModal from '@/components/IdleWarningModal';
 import '@/app/globals.css';
 
+export const metadata = {
+  title: 'Nascer com Registo',
+  description: 'Cidadania e Proteção desde o início',
+}
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt">
